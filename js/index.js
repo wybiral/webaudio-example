fca379b2e26eac2c71fdf6be49f0c4c5b9d07a6d
wybiral <davy.wybiral@gmail.com>
2016-07-16 12:54:59 -0500

    Create audio source with noise

--- js/index.js
+++ js/index.js
@@ -14,4 +14,30 @@ window.onload = function() {
 };
 
 function play(ctx, node) {
+    // Get current time in seconds
+    var startTime = ctx.currentTime;
+    // Set duration in seconds
+    var duration = 1.0;
+    // Get sample rate
+    var rate = ctx.sampleRate;
+    // Compute buffer length
+    var length = rate * duration;
+    // Create buffer
+    var buffer = ctx.createBuffer(1, length, rate);
+    // Grab channel for inserting samples
+    var data = buffer.getChannelData(0);
+
+    // Generate white noise
+    for (var i = 0; i < data.length; i++) {
+        data[i] = Math.random() * 2 - 1;
+    }
+
+    // Create audio source
+    var source = ctx.createBufferSource();
+    // Set buffer to our created one
+    source.buffer = buffer;
+    // Connect to gain node
+    source.connect(node);
+    // Play audio source at startTime
+    source.start(startTime);
 }

150e0a05cdb994e70505bd985870b08c92a19578
wybiral <davy.wybiral@gmail.com>
2016-07-16 13:06:24 -0500

    Added comment

--- js/index.js
+++ js/index.js
@@ -14,6 +14,7 @@ window.onload = function() {
 };
 
 function play(ctx, node) {
+    // Frequency of note
     var freq = 220.0;
     // Get current time in seconds
     var startTime = ctx.currentTime;

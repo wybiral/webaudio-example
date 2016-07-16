3ecb6de02007c6bb0f7da9949d883d21895b4d63
wybiral <davy.wybiral@gmail.com>
2016-07-16 12:53:33 -0500

    Setup audio context and play button

--- js/index.js
+++ js/index.js
@@ -1,2 +1,17 @@
 window.onload = function() {
+    // Create WebAudio context
+    var ctx = new AudioContext();
+    // Create gain node
+    var node = ctx.createGain();
+    // Connect gain node to context (output)
+    node.connect(ctx.destination);
+
+    // Add handler for play button
+    var btn = document.getElementById('play');
+    btn.onclick = function() {
+        play(ctx, node);
+    };
 };
+
+function play(ctx, node) {
+}
